31bce11b2c848d7a01ac33331ea9073cf1b4b31e
shivamjai800 <shivamjai800@gmail.com>
2020-07-24 15:54:39 +0530

    modified:   mac/__pycache__/settings.cpython-37.pyc 	modified:   mac/__pycache__/urls.cpython-37.pyc 	modified:   mac/settings.py 	modified:   mac/urls.py

--- mac/urls.py
+++ mac/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',view.index),
     path('shops/',include('shops.urls')),
     path('blog/',include('blog.urls' ))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
